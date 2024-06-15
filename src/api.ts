8505ff646b3168844594d7c17ce204f36610e5df
Michael Danielyan <32106337+eyerust@users.noreply.github.com>
2024-06-15 17:11:43 +0900

    Define API types

---
+++ src/api.ts
@@ -0,0 +1,13 @@
+export type noteId = {
+    username: string,
+    time: string,
+    messageText: string,
+    channelName: string
+}
+
+export type note = {
+    id: noteId,
+    text: string
+    color: string
+    creationTimestamp: string
+}
