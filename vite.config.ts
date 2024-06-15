7d15212a00c338f5a66c912500629b1acfc8b023
Michael Danielyan <32106337+eyerust@users.noreply.github.com>
2024-06-15 14:49:21 +0900

    Implement basic content.js

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),     viteStaticCopy({
+    targets: [
+      {
+        src: 'src/content.js',
+        dest: '.'
+      }
+    ]
+  }),         {
+    name: 'watch-external', // https://stackoverflow.com/questions/63373804/rollup-watch-include-directory/63548394#63548394
+    async buildStart(){
+        this.addWatchFile('src/content.js')
+    }
+}],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
