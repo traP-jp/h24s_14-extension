dfe41f96411c1070a7505b6d11a25bbd4cce7da4
rei <h.watanabe360@gmail.com>
2024-06-15 13:45:58 +0900

    cliplistのプロトタイプを作成

---
+++ src/components/ClipList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ClipItem from './ClipItem.vue'
+import { ref } from 'vue'
+
+const currentPage = ref(1)
+const paginationLength = 4
+const clipLintLength = 10
+</script>
+
+<template>
+  <v-list>
+    <clip-item v-for="i in clipLintLength" :key="i" />
+  </v-list>
+  <v-pagination v-model="currentPage" :length="paginationLength"></v-pagination>
+</template>
+
+<style></style>
---
+++ src/components/PopUp.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ClipList from './ClipList.vue'
+</script>
+
+<template>
+  <ClipList />
+</template>
+
+<style></style>
