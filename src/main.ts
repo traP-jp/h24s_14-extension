870ecf2238ba3f6952c42233c5b13d62f28ac863
Natsuki <qq973313484@gmail.com>
2024-06-15 10:41:24 +0900

    メインページに部分的にマウントするように環境構築した

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const container = document.getElementById('logo')
+container?.insertAdjacentHTML('beforebegin', `<div id="vue-app"></div>`)
+alert('ok!')
+
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.mount('#vue-app')
