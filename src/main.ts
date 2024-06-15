fde399deb4575d4da662fcf6f465ddad43043ece
Michael Danielyan <32106337+eyerust@users.noreply.github.com>
2024-06-15 11:26:21 +0900

    Fix

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,5 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-const container = document.getElementById('logo')
-container?.insertAdjacentHTML('beforebegin', `<div id="vue-app"></div>`)
-alert('ok!')
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#vue-app')
+createApp(App).mount('#app')
