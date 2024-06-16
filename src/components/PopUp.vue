ef7c621d822cba5eb2e2f6414f3f12de7de0f5ae
zer0-star <zer0star.65535@gmail.com>
2024-06-16 10:44:24 +0900

    ダークモード対応

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import { usePreferredColorScheme } from '@vueuse/core'
 import NoteList from './NoteList.vue'
+
+const theme = usePreferredColorScheme()
 </script>
 
 <template>
-  <NoteList />
+  <v-theme-provider :theme="theme" with-background>
+    <NoteList />
+  </v-theme-provider>
 </template>
 
 <style></style>
