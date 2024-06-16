48a24440acb849eb48890d68a5b8641794dd60b0
kyomu <torimitsu.s.aa@m.titech.ac.jp>
2024-06-16 12:26:42 +0900

    chore: ボタンのずれをわずかに修正

--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -45,7 +45,7 @@ const editNote = (id: noteId) => {
       <v-list-item-title>{{ note.text }}</v-list-item-title>
       <v-list-item-subtitle>
         <v-scroll-y-reverse-transition mode="out-in">
-          <div v-if="state.confirmDelete" class="text-success text-caption">
+          <div v-if="state.confirmDelete" class="text-caption">
             本当に削除しますか?
           </div>
           <div v-else class="text-caption">
@@ -55,18 +55,22 @@ const editNote = (id: noteId) => {
       </v-list-item-subtitle>
     </v-list-item-content>
     <template v-slot:append>
-      <v-fade-transition mode="out-in">
-        <v-row class="mt-3" justify="start" v-if="state.confirmDelete">
-          <v-btn size="small" variant="outlined" class="mr-2" @click="deleteNote(note.id)">Yes</v-btn>
-          <v-btn size="small" variant="outlined" @click="state.confirmDelete = false">No</v-btn>
-        </v-row>
+      <v-fade-transition mode="in-out">
+        <v-col class="d-flex justify-end">
+          <v-row class="mt-3" v-if="state.confirmDelete">
+            <v-btn size="small" variant="outlined" class="mr-2" @click="deleteNote(note.id)">Yes</v-btn>
+            <v-btn size="small" variant="outlined" @click="state.confirmDelete = false">No</v-btn>
+          </v-row>
+        </v-col>
       </v-fade-transition>
-      <v-fade-transition mode="out-in">
-        <v-row class="mt-3" justify="start" v-if="!state.confirmDelete">
-          <v-btn size="small" variant="outlined" class="mr-2" @click="state.confirmDelete = true">delete</v-btn>
-          <v-btn size="small" variant="outlined" @click="editNote(note.id)">edit</v-btn>
-        </v-row>
-      </v-fade-transition>
+      <v-transition mode="in-out">
+        <v-col class="d-flex justify-end">
+          <v-row class="mt-3" v-if="!state.confirmDelete">
+            <v-btn size="small" variant="outlined" class="mr-2" @click="state.confirmDelete = true">削除</v-btn>
+            <v-btn size="small" variant="outlined" @click="editNote(note.id)">編集</v-btn>
+          </v-row>
+        </v-col>
+      </v-transition>
     </template>
   </v-list-item>
 </template>
